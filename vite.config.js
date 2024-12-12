cbf9b9a0e7f1a8502a1ba7a7516f04696e7b8b4f
kumarram27 <115088025+kumarram27@users.noreply.github.com>
2024-12-12 18:42:20 +0530

    chore: Simplify package.json scripts and update Vite config for cleaner build process

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html",
-        background: "public/background.js", 
-      },
-      output: {
-        entryFileNames: "assets/[name].js",
-        chunkFileNames: "assets/[name].js",
-        assetFileNames: "assets/[name].[ext]",
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: "index.html",
+  //       background: "public/background.js",
+  //     },
+  //     output: {
+  //       entryFileNames: "assets/[name].js",
+  //       chunkFileNames: "assets/[name].js",
+  //       assetFileNames: "assets/[name].[ext]",
+  //     },
+  //   },
+  // },
 });
